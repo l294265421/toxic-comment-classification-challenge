2f59897757555d7a7d451bf4ecfd432d3fd3c022
yuncong_li <yuncong_li@kingdee.com>
2018-03-06 17:56:41 +0800

    feat

--- base/logistic_classifier.py
+++ base/logistic_classifier.py
@@ -8,6 +8,7 @@ from nltk.stem import SnowballStemmer
 from nltk.stem import PorterStemmer
 from nltk import pos_tag, word_tokenize
 from scipy.sparse import hstack
+from sklearn.model_selection import KFold
 
 english_stemmer = SnowballStemmer('english')
 def normalize_word(word):
@@ -23,18 +24,37 @@ class StemmedTfidfVectorizer(TfidfVectorizer):
         analyzer = super(StemmedTfidfVectorizer, self).build_analyzer()
         return lambda doc: (normalize_word(w) for w in analyzer(doc))
 
-v = StemmedTfidfVectorizer(stop_words='english', tokenizer=word_tokenize, ngram_range=(1, 3), max_features=15000)
+train_text = train_df['comment_text']
+test_text = test_df['comment_text']
+all_text = pd.concat([train_text, test_text])
 
-X = v.fit_transform(train_df['comment_text'])
-X_test = v.transform(test_df['comment_text'])
+v = StemmedTfidfVectorizer(stop_words='english', tokenizer=word_tokenize, ngram_range=(1, 1), max_features=15000)
+v.fit(all_text)
+X1 = v.transform(train_df['comment_text'])
+X1_test = v.transform(test_df['comment_text'])
 
-X = hstack([X, train_statistics])
-X_test = hstack([X_test, test_statistics])
+v = StemmedTfidfVectorizer(stop_words='english', tokenizer=word_tokenize, ngram_range=(2, 3), max_features=5000)
+v.fit(all_text)
+X2 = v.transform(train_df['comment_text'])
+X2_test = v.transform(test_df['comment_text'])
+
+char_vectorizer = TfidfVectorizer(
+    sublinear_tf=True,
+    strip_accents='unicode',
+    analyzer='char',
+    ngram_range=(1, 5),
+    max_features=15000)
+char_vectorizer.fit(all_text)
+train_char_features = char_vectorizer.transform(train_text)
+test_char_features = char_vectorizer.transform(test_text)
+
+X = hstack([X1, X2, train_char_features])
+X_test = hstack([X1_test, X2_test, test_char_features])
 
 aucs = []
 for label in ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']:
     y = train_df[label]
-    X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.05, random_state=1234)
+    X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.3, random_state=2345)
     model = LogisticRegression()
     model.fit(X_train, y_train)
     aucs.append(roc_auc_score(y_validation, model.predict_proba(X_validation)[:, 1]))
---
+++ base/rnn_cnn.py
@@ -0,0 +1,118 @@
+import numpy as np
+
+np.random.seed(42)
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+
+from keras.models import Model
+from keras.layers import Input, Dense, Embedding, SpatialDropout1D, concatenate
+from keras.layers import GRU, LSTM, Bidirectional, GlobalAveragePooling1D, GlobalMaxPooling1D
+from keras.preprocessing import text, sequence
+from keras.callbacks import Callback
+from keras.callbacks import EarlyStopping
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.models import Sequential
+from keras.layers import Convolution1D, MaxPooling1D
+from keras.layers.convolutional import Conv1D
+
+import warnings
+
+warnings.filterwarnings('ignore')
+
+import os
+
+import gensim
+
+from data.raw_data import *
+
+os.environ['OMP_NUM_THREADS'] = '4'
+
+EMBEDDING_FILE = base_dir + 'crawl-300d-2M.vec'
+
+train = train_df
+test = test_df
+submission = submission
+
+X_train = train["comment_text"].fillna("fillna").values
+y_train = train[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]].values
+X_test = test["comment_text"].fillna("fillna").values
+
+max_features = 30000
+maxlen = 300
+embed_size = 300
+
+tokenizer = text.Tokenizer(num_words=max_features)
+tokenizer.fit_on_texts(list(X_train) + list(X_test))
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+x_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+
+def get_coefs(word, *arr): return word, np.asarray(arr, dtype='float32')
+
+word_index = tokenizer.word_index
+
+embeddings_index = dict(get_coefs(*o.strip().split()) for o in open(EMBEDDING_FILE, encoding='utf-8') if o.strip().split()[0] in word_index)
+
+all_embs = np.stack(embeddings_index.values())
+emb_mean, emb_std = all_embs.mean(), all_embs.std()
+
+nb_words = min(max_features, len(word_index) + 1)
+embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+for word, i in word_index.items():
+    if i >= max_features: continue
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+
+
+class RocAucEvaluation(Callback):
+    def __init__(self, validation_data=(), interval=1):
+        super(Callback, self).__init__()
+
+        self.interval = interval
+        self.X_val, self.y_val = validation_data
+
+    def on_epoch_end(self, epoch, logs={}):
+        if epoch % self.interval == 0:
+            y_pred = self.model.predict(self.X_val, verbose=0)
+            score = roc_auc_score(self.y_val, y_pred)
+            print("\n ROC-AUC - epoch: %d - score: %.6f \n" % (epoch + 1, score))
+
+def get_model():
+    sequence_input = Input(shape=(maxlen,))
+    x = Embedding(nb_words, embed_size, weights=[embedding_matrix], trainable=False)(sequence_input)
+    x = SpatialDropout1D(0.2)(x)
+    x = Bidirectional(LSTM(128, return_sequences=True, dropout=0.1, recurrent_dropout=0.1))(x)
+    x = Conv1D(64, kernel_size=2, padding="valid", kernel_initializer="glorot_uniform")(x)
+    avg_pool = GlobalAveragePooling1D()(x)
+    max_pool = GlobalMaxPooling1D()(x)
+    x = concatenate([avg_pool, max_pool])
+    # x = Dense(128, activation='relu')(x)
+    # x = Dropout(0.1)(x)
+    preds = Dense(6, activation="sigmoid")(x)
+    model = Model(sequence_input, preds)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    return model
+
+
+model = get_model()
+
+batch_size = 1024
+epochs = 10
+
+X_tra, X_val, y_tra, y_val = train_test_split(x_train, y_train, train_size=0.95 , random_state=233)
+RocAuc = RocAucEvaluation(validation_data=(X_val, y_val), interval=1)
+
+#define callbacks
+early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=1, verbose=1)
+callbacks_list = [early_stopping, RocAuc]
+
+hist = model.fit(X_tra, y_tra, batch_size=batch_size, epochs=epochs, validation_data=(X_val, y_val),
+                 callbacks=callbacks_list, verbose=2)
+
+y_pred = model.predict(x_test, batch_size=1024)
+submission[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]] = y_pred
+submission.to_csv(base_dir + 'cnn_rnn.csv', index=False)
